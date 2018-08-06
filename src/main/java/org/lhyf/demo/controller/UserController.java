4ac829008470dce0b92c1949ed34b37bb0adfb6a
花草便当 <lhyf@outlook.com>
2018-08-06 09:55:34 +0800

    .

--- src/main/java/org/lhyf/demo/controller/UserController.java
+++ src/main/java/org/lhyf/demo/controller/UserController.java
@@ -4,7 +4,6 @@ import org.lhyf.demo.entity.TUser;
 import org.lhyf.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
